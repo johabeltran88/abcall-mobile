cdaa04c4207c9fb490786920da7cf00326ea043f
Johanna Beltran <ja.beltrang12@uniandes.edu.co>
2024-10-10 20:14:21 -0500

    Agregar servicio sin funcionar falta integracion

---
+++ app/src/main/java/com/example/test/model/Consumer.kt
@@ -0,0 +1,6 @@
+package com.example.test.model
+
+data class Consumer(
+    val email: String?,
+    val password: String?,
+)
---
+++ app/src/main/java/com/example/test/model/Token.kt
@@ -0,0 +1,5 @@
+package com.example.test.model
+
+data class Token(
+    val token: String?,
+)
